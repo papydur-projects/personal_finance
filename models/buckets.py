d47d06448a50ed204e52a651f4bd8440ca91c308
papydur <joostvdp92@gmail.com>
2022-05-27 14:43:21 +0200

    refactor Bucket class

--- models/buckets.py
+++ models/buckets.py
@@ -1,16 +1,16 @@
 from abc import ABC, abstractmethod
 from pycoingecko import CoinGeckoAPI
-from pydantic import BaseModel, Field, ValidationError
+from pydantic import BaseModel, Field
 import datetime
-from typing import List, Set
-from collections.abc import Iterable
+from typing import List, Set, Type, Optional
 
-from models.assets import CashAsset, Asset
+from models.assets import CashAsset, Asset, CryptoAsset
 
 
 class Bucket(ABC, BaseModel):
     assets: List = Field(default_factory=list)
     date: datetime.date = datetime.date.today()
+    allowed_asset_type: Type[Asset] = Field(init=False)
     total_value: float = Field(init=False, default=None)
 
     def get_total_value(self):
@@ -19,39 +19,37 @@ class Bucket(ABC, BaseModel):
             total_value += asset.total_value
         return total_value
 
-    @staticmethod
-    def validate_assets(assets, asset_type):
-        if not isinstance(assets, Iterable):
-            raise ValueError('function expects list of assets')
-        if not all(isinstance(asset, asset_type) for asset in assets):
-            raise ValueError(f'not all assets were of type {asset_type}')
+    def validate_asset(self, asset):
+        if not isinstance(asset, self.allowed_asset_type):
+            raise TypeError(f'The added asset is not of type {self.allowed_asset_type}')
 
-    @abstractmethod
-    def add_assets(self, assets: Iterable[Asset]):
-        self.assets.extend(assets)
+    def add_asset(self, asset):
+        self.validate_asset(asset)
+        self.assets.append(asset)
 
     def change_date(self, year, month, day):
         self.date = datetime.date(year, month, day)
 
 
 class CashBucket(Bucket):
-    def add_assets(self, assets):
-        self.validate_assets(assets, CashAsset)
-        super().add_assets(assets)
+    allowed_asset_type: Type[CashAsset] = CashAsset
 
 
 class CryptoBucket(Bucket):
+    allowed_asset_type: Type[CryptoAsset] = CryptoAsset
     cgapi: CoinGeckoAPI = Field(init=False, default=None)
     supported_tickers: Set = Field(init=False, default=None)
 
     class Config:
         arbitrary_types_allowed = True
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         self.cgapi = CoinGeckoAPI()
         self.supported_tickers = self.get_supported_coin_tickers()
 
-    def get_supported_coin_tickers(self):
+    def get_supported_coin_tickers(self) -> Set:
         coins_list = self.cgapi.get_coins_list()
         supported_tickers = {item['symbol'] for item in coins_list}
         return supported_tickers
+
+
--- tests/test_models/test_bucket.py
+++ tests/test_models/test_bucket.py
@@ -1,6 +1,4 @@
 import pytest
-from pydantic import ValidationError
-
 from models.assets import CashAsset, CryptoAsset
 
 
@@ -8,25 +6,31 @@ class TestCashBucket:
     def test_constructor(self, empty_cash_bucket):
         assert empty_cash_bucket
 
+    def test_allowed_asset_type(self, empty_cash_bucket):
+        assert empty_cash_bucket.allowed_asset_type == CashAsset
+
     def test_add_one_asset(self, empty_cash_bucket):
-        assets = [CashAsset('cash', 1000)]
-        empty_cash_bucket.add_assets(assets)
+        asset = CashAsset('cash', 1000)
+        empty_cash_bucket.add_asset(asset)
         assert len(empty_cash_bucket.assets) == 1
 
-    def test_add_asset_list(self, empty_cash_bucket, cash_assets):
-        empty_cash_bucket.add_assets(cash_assets)
+    def test_add_two_assets(self, empty_cash_bucket):
+        asset = CashAsset('cash', 1000)
+        empty_cash_bucket.add_asset(asset)
+        asset = CashAsset('debt', -100)
+        empty_cash_bucket.add_asset(asset)
         assert len(empty_cash_bucket.assets) == 2
 
-    def test_add_asset_validation(self, empty_cash_bucket):
-        with pytest.raises(ValueError):
-            assets = CashAsset('cash', 1000)
-            empty_cash_bucket.add_assets(assets)
+    def test_add_wrong_asset_type(self, empty_cash_bucket):
+        with pytest.raises(TypeError) as e:
+            asset = CryptoAsset('bitcoin', 1000, 'btc')
+            empty_cash_bucket.add_asset(asset)
 
-    def test_cash_instances_validation(self, empty_cash_bucket):
-        with pytest.raises(ValueError):
-            assets = [CashAsset('cash', 1000),
-                      CryptoAsset('bitcoin', 100, 'btc')]
-            empty_cash_bucket.add_assets(assets)
+
+class TestCryptoBucket:
+    pass
+
+
 
 
 
